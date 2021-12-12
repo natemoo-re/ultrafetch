2ded3e92ebaa19116be08d757ee196b6f42d1135
Nate Moore <nate@skypack.dev>
2021-12-12 08:47:58 -0600

    chore: run prettier

--- test/basic.ts
+++ test/basic.ts
@@ -1,36 +1,36 @@
-import createFetch from '../dist/index.js';
-import { test } from 'uvu';
-import * as assert from 'uvu/assert';
-import * as utils from './utils';
+import createFetch from "../dist/index.js";
+import { test } from "uvu";
+import * as assert from "uvu/assert";
+import * as utils from "./utils";
 
 const fetch = createFetch();
 
 test.before(async () => {
-    await utils.cleanup()
-    assert.not(utils.cacheExists())
-})
-
-test('exports', () => {
-	assert.type(createFetch, 'function');
-    assert.type(fetch, 'function');
+  await utils.cleanup();
+  assert.not(utils.cacheExists());
 });
 
-test('example', async () => {
-    const res = await fetch('https://example.com');
-    assert.equal(res.status, 200);
+test("exports", () => {
+  assert.type(createFetch, "function");
+  assert.type(fetch, "function");
 });
 
-test('file', async () => {
-    const res = await fetch(new URL('../package.json', import.meta.url));
-    assert.equal(res.status, 200);
-    const data = await res.json();
-    assert.equal(data.name, 'ultrafetch');
+test("example", async () => {
+  const res = await fetch("https://example.com");
+  assert.equal(res.status, 200);
 });
 
-test('file 404', async () => {
-    const res = await fetch(new URL('../does-not-exist.ts', import.meta.url));
-    assert.equal(res.ok, false);
-    assert.equal(res.status, 404);
+test("file", async () => {
+  const res = await fetch(new URL("../package.json", import.meta.url));
+  assert.equal(res.status, 200);
+  const data = await res.json();
+  assert.equal(data.name, "ultrafetch");
 });
 
-test.run()
+test("file 404", async () => {
+  const res = await fetch(new URL("../does-not-exist.ts", import.meta.url));
+  assert.equal(res.ok, false);
+  assert.equal(res.status, 404);
+});
+
+test.run();
--- test/utils.ts
+++ test/utils.ts
@@ -1,12 +1,16 @@
-import { statSync } from 'fs'
-import del from 'del'
-import { fileURLToPath } from 'url'
+import { statSync } from "fs";
+import del from "del";
+import { fileURLToPath } from "url";
 
-export const cleanup = () => del(fileURLToPath(new URL('../node_modules/.cache', import.meta.url)));
+export const cleanup = () =>
+  del(fileURLToPath(new URL("../node_modules/.cache", import.meta.url)));
 export const cacheExists = () => {
-    try {
-        return statSync(fileURLToPath(new URL('../node_modules/.cache', import.meta.url))).isDirectory();
-    } catch (e) {}
-    return false;
-}
-export const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+  try {
+    return statSync(
+      fileURLToPath(new URL("../node_modules/.cache", import.meta.url))
+    ).isDirectory();
+  } catch (e) {}
+  return false;
+};
+export const sleep = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
